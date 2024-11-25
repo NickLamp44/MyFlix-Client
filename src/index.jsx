adbee44dc7e5710bae2109050a5f1886a98105b5
OGlamp <lamparellin@gmail.com>
2024-11-25 17:37:45 -0500

    bugfix

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
 
 // Import statement to indicate that you need to bundle `index.scss`
-import { MainView } from "./components/MainView/mainView";
+import { MainView } from "./components/MainView/mainView.jsx";
 import { Container } from "react-bootstrap";
 import "./index.scss";
 
