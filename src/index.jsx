5f9bb73f59420d5c5803c077b29177ef1f8497d9
OGlamp <lamparellin@gmail.com>
2024-11-25 17:43:44 -0500

    Build Fail
    attempting to debug a deploy issue with netlify & Vercal

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,9 @@
 import { createRoot } from "react-dom/client";
 
-// Import statement to indicate that you need to bundle `index.scss`
 import { MainView } from "./components/MainView/mainView.jsx";
 import { Container } from "react-bootstrap";
 import "./index.scss";
 
-// Import statement to indicate that you need to bundle `index.scss`
 const MyFlixApp = () => {
   return (
     <Container>
@@ -14,9 +12,7 @@ const MyFlixApp = () => {
   );
 };
 
-// Find the root of your app
 const container = document.querySelector("#root");
 const root = createRoot(container);
 
-// Tell React to render the app in the root DOM element
 root.render(<MyFlixApp />);
