9d973b9142db0744a3df16c23906e4aaea6414c5
OGlamp <lamparellin@gmail.com>
2024-11-26 13:11:53 -0500

    rebuild

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
 
-import { MainView } from "./components/MainView/mainView.jsx";
+import { MainView } from "./components/mainView/mainView.jsx";
 import { Container } from "react-bootstrap";
 import "./index.scss";
 
